2fd9a6cbe36bb3a7381c1bed4d23c96932f74e1f
Marc Gibbons <marc.gibbons@bnotions.com>
2013-07-12 23:45:28 -0400

    Updated settings

--- cigar_example/cigar_example/settings.py
+++ cigar_example/cigar_example/settings.py
@@ -58,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/var/www/swagger/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
