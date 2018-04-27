b738042d9b7d5ea5984744b0fb729d753ebc51e7
Marc Gibbons <marc_gibbons@rogers.com>
2018-04-27 14:17:01 -0400

    Set CSRF token to HTTP requests when present

--- client/index.js
+++ client/index.js
@@ -2,7 +2,18 @@ import SwaggerUI from 'swagger-ui'
 import 'swagger-ui/dist/swagger-ui.css';
 import './styles.css'
 
+
 window.drsSettings.dom_id = '#rest-swagger-ui';
 window.drsSettings.spec = window.drsSpec;
+const csrfTokenInput = window.document.getElementsByName('csrfmiddlewaretoken');
+
+// If present, set CSRF token to request headers
+if (csrfTokenInput.length) {
+  const csrfToken = csrfTokenInput[0].value;
+  window.drsSettings.requestInterceptor = (req) => {
+    req.headers['X-CSRFToken'] = csrfToken;
+    return req;
+  }
+}
 
 window.swagger = SwaggerUI(window.drsSettings);
