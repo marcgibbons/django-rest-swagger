3467f5481b0796dac59858d59e8871facd9d79b0
Marc Gibbons <marc_gibbons@rogers.com>
2018-04-30 11:28:01 -0400

    Add dev server, update templates

--- client/index.js
+++ client/index.js
@@ -1,19 +1,22 @@
 import SwaggerUI from 'swagger-ui'
 import 'swagger-ui/dist/swagger-ui.css';
-import './styles.css'
+import './styles.css';
 
+let config = {
+  dom_id: '#rest-swagger-ui',
+  spec: window.drsSpec,
+};
 
-window.drsSettings.dom_id = '#rest-swagger-ui';
-window.drsSettings.spec = window.drsSpec;
 const csrfTokenInput = window.document.getElementsByName('csrfmiddlewaretoken');
 
 // If present, set CSRF token to request headers
 if (csrfTokenInput.length) {
   const csrfToken = csrfTokenInput[0].value;
-  window.drsSettings.requestInterceptor = (req) => {
+  config.requestInterceptor = (req) => {
     req.headers['X-CSRFToken'] = csrfToken;
     return req;
   }
 }
 
-window.swagger = SwaggerUI(window.drsSettings);
+
+window.swagger = SwaggerUI(Object.assign(config, window.drsSettings));
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,8 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const webpack = require('webpack');
+const devMode = process.env.NODE_ENV === 'dev';
 
 const distPath = path.resolve(
   __dirname,
@@ -13,7 +15,8 @@ module.exports = {
   },
   output: {
     path: distPath,
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
+    publicPath: '/static/rest_framework_swagger/dist/',
   },
   module: {
     rules: [
@@ -23,13 +26,21 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           'css-loader'
         ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
       }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(distPath),
     new MiniCssExtractPlugin({
       filename: '[name].bundle.css',
-    }),
+    })
   ],
   optimization: {
     splitChunks: {
